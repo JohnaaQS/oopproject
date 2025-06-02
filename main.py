0d4051ac4799e3506b25322a3773f3fe01f72843
Johnaa <johnatansno@gmail.com>
2025-06-02 12:39:48 +0200

    hh

---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import sys
+import terrein
+from mario import Mario
+import koepa
+
+
+# Init
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mario Mannetje (1 afbeelding)")
+blokken = terrein.Blocks()
+
+
+# Clock
+clock = pygame.time.Clock()
+FPS = 60
+
+
+# Kleuren
+bg = pygame.image.load('images\Achtergrond.png')
+
+
+# Maak sprites
+player = Mario(100, HEIGHT - 150)
+enemy = koepa.KoepaTroepa(300, HEIGHT - 150)
+
+koepa_group = pygame.sprite.Group()
+koepa_group.add(enemy)
+
+mario_group = pygame.sprite.Group()
+mario_group.add(player)
+
+# Main loop
+run = True
+while run:
+    clock.tick(FPS)
+    screen.blit(bg, (0,-330))
+    keys = pygame.key.get_pressed()
+    
+    mario_group.update(keys, koepa_group)
+    player = mario_group.sprites()[0]
+    if player.dood:
+        run = False  # Of toon een game over scherm
+        
+    
+    mario_group.draw(screen)
+    koepa_group.update()
+    koepa_group.draw(screen)
+    
+    blokken.draw(screen)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
